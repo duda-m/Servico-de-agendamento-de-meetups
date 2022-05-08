88900c189ff8a38a397ef40b6cd30e12bc051715
duda-m <dudadev35@gmail.com>
2022-05-08 18:03:27 -0300

    undoing the changing in basePackage

--- src/main/java/com/bootcamp/microservicemeetup/config/SwaggerConfig.java
+++ src/main/java/com/bootcamp/microservicemeetup/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis( RequestHandlerSelectors.basePackage("com.bootcamp.microservicemeetup.controller.resources") )
+                .apis( RequestHandlerSelectors.basePackage("com.womakerscode.microservicemeetup.Servico.de.Agendamento.de.Meetups.controller.resource") )
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
